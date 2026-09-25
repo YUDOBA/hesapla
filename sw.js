aaaca3827685609ee907dc8bafc6ec50cda8f26b
YUDOBA <46269442+YUDOBA@users.noreply.github.com>
2026-09-25 11:44:31 +0300

    V1 service worker

--- sw.js
+++ sw.js
@@ -1,8 +1,15 @@
-const CACHE = "hesapla-v0";
-const ASSETS = ["./", "index.html", "style.css", "app.js", "manifest.webmanifest"];
+const CACHE = "hesapla-v1";
+const CORE = ["./index.html", "style.css", "app.js", "manifest.webmanifest", "icon.svg"];
+const EXTRA = ["icon-192.png", "icon-512.png", "apple-touch-icon.png", "./"];
 
 self.addEventListener("install", function (e) {
-  e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(ASSETS); }));
+  e.waitUntil(
+    caches.open(CACHE).then(function (c) {
+      return c.addAll(CORE).then(function () {
+        return Promise.all(EXTRA.map(function (u) { return c.add(u).catch(function () {}); }));
+      });
+    })
+  );
   self.skipWaiting();
 });
 
